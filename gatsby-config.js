f19605d5eb27ee42c13cb3e86e23eb05e9e958b2
zwelinux <zwelinux@gmail.com>
2023-01-25 08:57:01 +0630

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-demo-1`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
